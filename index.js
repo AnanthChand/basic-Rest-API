dd178b0d02debe94d5ddf050017b381138a34235
Ananth Chandra <ananthinindia@gmail.com>
2023-01-12 22:45:39 +0530

    final commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const bodyparcer = require("body-parser")
+const bodyparser = require("body-parser")
 const app = express();
 const PORT = 3000;
 
 
-app.use(bodyparcer.json());
+app.use(bodyparser.urlencoded({ extended: true }))
 
 
 app.get("/", (req, res) => {
