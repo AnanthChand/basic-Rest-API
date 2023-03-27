ef32bba44236795b92a0608180cf69a6e3135dbf
Ananth Chandra <ananthinindia@gmail.com>
2023-03-27 14:09:37 +0530

    bug fixes

--- index.js
+++ index.js
@@ -8,40 +8,40 @@ app.use(bodyparser.json());
 app.set("view engine", "ejs");
 
 var bmi = "";
+var bmiConsequence =''
 
 app.get("/", (req, res) => {
   console.log("Testing api");
-  res.render("index", { bmiResult: bmi });
+  res.render("index", { bmiResult: bmi,bmiConsequence:bmiConsequence });
 });
 
 app.post("/", (req, res) => {
   const weight = Number(req.body.weight);
   const height = Number(req.body.height);
   bmi = (weight / (height * height)) * 10000;
+
+
+//   if(((weight / (height * height)) * 10000)<18.5)
+
   if (bmi < 18.5) {
-    bmi =
+    bmiConsequence =
       "The BMI of your body is: " +
       bmi +
       ".And thus you come as an underweight person.";
   }
   if (bmi > 18.5 && bmi < 24.9) {
-    bmi =
+    bmiConsequence =
       "The BMI of your body is: " +
       bmi +
       ". And thus you come as a healthy person.";
   } else if (bmi > 24.9) {
-    bmi =
+    bmiConsequence =
       "The BMI of your body is: " +
       bmi +
       ". And thus you come as an overweight person.";
   }
 
-  // res.end()
-  // console.log(req.body);
-  // console.log(bmi);
-  // document.getElementById("BmiResult").innerHTML = 'The BMI of your body is: ${bmi}'  ;
-  // res.render('index',{bmiResult:bmi});
-
+ 
   res.redirect("/");
 });
 
